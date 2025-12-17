b7b7e8ee93e4d6f5782592871a8ff30dd11f897f
HR18HR <163581496+HR18HR@users.noreply.github.com>
2025-12-17 17:07:51 +0100

    Update capacitor.config.ts

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.alertup',
   appName: 'AlertUp',
-  webDir: 'dist/frontend/browser',
+  webDir: 'dist/frontend',
   plugins: {
     CapacitorHttp: {
       enabled: true
